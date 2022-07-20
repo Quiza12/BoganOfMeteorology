d8b41326b0f6565a1154cb45facba43dabc3fa5e
Quiza12 <Quiza12@live.com>
2022-07-20 20:44:39 +1000

    Testing all working with hardcoded SAS token (to remove once verified).

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  azureSas: 'sp=r&st=2022-07-17T06:36:34Z&se=2022-07-17T14:36:34Z&spr=https&sv=2021-06-08&sr=c&sig=uGki%2FlD0BOyfsLqXZanwUBOxcf4ccSrbKXTnQ3Vmjhk%3D'
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  azureSas: 'sp=r&st=2022-07-17T06:36:34Z&se=2022-07-17T14:36:34Z&spr=https&sv=2021-06-08&sr=c&sig=uGki%2FlD0BOyfsLqXZanwUBOxcf4ccSrbKXTnQ3Vmjhk%3D'
+  azureSas: 'sp=r&st=2022-07-20T10:42:04Z&se=2022-07-20T18:42:04Z&spr=https&sv=2021-06-08&sr=c&sig=TdMKqutu26Y8Kkdny9a%2F3muZ39T73%2F17Ual8cJnfJnI%3D'
 };
 
 /*
