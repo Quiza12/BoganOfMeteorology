7109223e9fdaaf08c81cf38f0523e4087cd5a1b6
Quiza12 <Quiza12@live.com>
2023-05-23 20:34:19 +1000

    Updating blob link

--- src/app/services/bom-data.service.ts
+++ src/app/services/bom-data.service.ts
@@ -6,10 +6,7 @@ import * as moment from 'moment';
 import { BlobServiceClient } from "@azure/storage-blob";
 import { environment } from '../../environments/environment';
 
-const account = "mqbomstorageaccount";
-const sas = "sp=r&st=2022-07-17T06:36:34Z&se=2022-07-17T14:36:34Z&spr=https&sv=2021-06-08&sr=c&sig=uGki%2FlD0BOyfsLqXZanwUBOxcf4ccSrbKXTnQ3Vmjhk%3D";
-
-const blobServiceClient = new BlobServiceClient('https://' + account + '.blob.core.windows.net/bom?' + environment.azureSas);
+const blobServiceClient = new BlobServiceClient('https://mqbomstorageaccount.blob.core.windows.net/bom?');
 
 @Injectable({
   providedIn: 'root'
