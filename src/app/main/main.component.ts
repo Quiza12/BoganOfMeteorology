dfbb79918364c239d36b97d84a8f5a41a9f334a7
Quiza12 <Quiza12@live.com>
2023-05-23 20:27:49 +1000

    Updating blob storage link.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
 enableProdMode();
 
